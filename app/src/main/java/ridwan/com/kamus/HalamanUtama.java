3d65d3e043197d0b8ddc0edac1295054b8810b1a
cholid_ridwan <cholid@id.edge-asia.com>
2017-03-15 13:19:36 +0700

    first commit, create halaman utama

---
+++ app/src/main/java/ridwan/com/kamus/HalamanUtama.java
@@ -0,0 +1,13 @@
+package ridwan.com.kamus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HalamanUtama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_utama);
+    }
+}
